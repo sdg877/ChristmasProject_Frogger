5e084554a5f2f3c0c2cbfef929f5e9c9efd81cdb
sdg877 <sdrakegill@gmail.com>
2023-12-24 07:49:52 +0000

    cars moving correctly

--- js/app.js
+++ js/app.js
@@ -5,17 +5,20 @@ function init () {
     const grid = document.querySelector('.grid')
 
     //board config
-    const width = 8
-    const height = 8
+    const width = 10
+    const height = 10
     const cellCount = width * height
     let cells = []
 
 
     //character config
-    const startingPosition = 60
+    const startingPosition = 95
     let currentPosition = startingPosition
     const carSpeed = 1000
-    const startingPositions = [32, 40, 48]
+    const validRows = [5, 6, 7, 8]
+    const fixedStartingPositions = [50, 60, 70, 80]
+    const lilyStartPositions = [10, 20, 30, 40]
+    let direction = "right"
 
 
     //! functions
@@ -33,6 +36,8 @@ function init () {
         }
 
         addFrog(startingPosition)
+        createCars()
+        moveCars()
     }
 
     function addFrog(position){
@@ -48,61 +53,160 @@ function init () {
             if (cells[position]) {
                 cells[position].classList.add('carright')
             } 
-            }
         }
+    }
 
-        function removeCar(position) {
+    function removeCar(position) {
             if (cells[position] && cells[position].classList.contains('carright')){
                 cells[position].classList.remove('carright')
             }
         }
 
-    function createCars() {
-        startingPositions.forEach((position, index) => {
-            setTimeout(() => {
-             const carMoveInterval = setInterval(() => {
-                const emptySpaces = cells.filter(
-                    (cell, idx) =>
-                    !cell.classList.contains('carright') &&
-                    Math.abs(position - idx) > 2 &&
-                    Math.floor(position / width) === Math.floor(idx / width)
-                )
+    function  isValidRow(position) {
+        const row = Math.floor(position / width)
+        return validRows.includes(row)
+    } 
 
-                if (emptySpaces.length > 0) {
-                    const randomEmptySpace =
-                    emptySpaces[Math.floor(Math.random() * emptySpaces.length)]
+    function hasSpaceAround(position) {
+        const adjacentPositions = [
+            position - width,
+            position - 1,
+            position + 1,
+            position + width
+        ]
 
-                    removeCar(position)
-                    addCar(randomEmptySpace.dataset.index)
-                    position = parseInt(randomEmptySpace.dataset.index, 10)
-                }
-            }, carSpeed)
-
-            setTimeout(() => {
-                clearInterval(carMoveInterval)
-            }, carSpeed * 30)
-        }, index * 3000)
-    })
-}
-
-
-    function moveCars() {
-        startingPositions.forEach((position, index) => {
-            removeCar(position)
-            const nextPosition = position + 1
-            if (nextPosition % width !== 0) {
-             startingPositions[index] = nextPosition
-              } else {
-               startingPositions[index] = Math.floor(nextPosition / width) * width
-            }
-            addCar(startingPositions[index])
-        })
+        return adjacentPositions.every(
+            (adjPos) =>
+            adjPos >= 0 &&
+            adjPos < cellCount &&
+            !cells[adjPos].classList.contains('carright')
+        )
     }
+
 
     function startCarMovements() {
         setInterval(moveCars, carSpeed)
-    }
+        }
 
+    function createCars() {
+         const carGenerationInterval = setInterval(() => {
+            const emptySpaces = cells.filter(
+                (cell, idx) =>
+                !cell.classList.contains('carright') &&
+                isValidRow(idx) &&
+                hasSpaceAround(idx)
+                );
+            if (emptySpaces.length > 0) {
+                const randomEmptySpace =
+                        emptySpaces[Math.floor(Math.random() * emptySpaces.length)]
+                addCar(Number(randomEmptySpace.dataset.index))
+            } else {
+                 clearInterval(carGenerationInterval)
+            }
+         }, carSpeed * 3) 
+        
+        setTimeout(() => {
+            clearInterval(carGenerationInterval)
+        }, carSpeed * 30)
+    }
+    
+    
+function moveCars() {
+     const carIndices = cells.reduce((acc, cell, index) => {
+            if (cell.classList.contains('carright') && isValidRow(index)) {
+                cells[index].classList.remove('carright')
+                acc.push(index)
+            }
+            return acc
+        }, [])
+
+        carIndices.forEach((currentPosition) => {
+            cells[currentPosition].classList.remove('carright')
+
+            let nextPosition = currentPosition + 1
+
+            if (nextPosition % width !== 0 && !cells[nextPosition].classList.contains('carright')) {
+                cells[nextPosition].classList.add('carright')
+            } else {
+                const currentRow = Math.floor(currentPosition / width)
+
+                const validRowIndex = validRows.indexOf(currentRow)
+
+                if (validRowIndex !== -1) {
+                    const nextValidRowIndex = (validRowIndex + 1) % validRows.length
+                    const nextRow = validRows[nextValidRowIndex]
+                    nextPosition = nextRow * width
+                    cells[nextPosition].classList.add('carright')
+                }
+            }
+        })
+
+    setTimeout(() => {
+        clearInterval(carMoveInterval)
+        cells.forEach((cell) => {
+            if (cell.classList.contains('carright')) {
+                cell.classList.remove('carright')
+            }
+        })
+    }, carSpeed * 30)
+}
+
+
+//         fixedStartingPositions.forEach((position) => {
+//                 removeCar(position)
+
+//              const nextPosition = position + 1
+
+//              if (nextPosition % width !== 0) {
+//                 addCar(nextPosition)
+//              } else {
+//                 removeCar(position)
+//              }
+//         })
+//     }, carSpeed)
+
+//     setTimeout(() => {
+//         clearInterval(carMoveInterval)
+//     }, carSpeed * 30)
+// }
+
+function addLilyPads() {
+    lilyStartPositions.forEach((startPosition, index) => {
+        let position = startPosition
+
+        const lilyInterval = setInterval(() => {
+        const addLily = () => {
+            if (position >= 0 && position < cellCount) {
+                cells[position].classList.add('lilypad')
+            }
+        }
+        const removeLily = () => {
+            if (position >= 0 && position < cellCount) {
+                cells[position].classList.remove('lilypad')
+            }
+        }
+
+        addLily()
+
+        const moveLily = () => {
+            removeLily()
+            position--
+
+            if (position < 0 || position % width === width - 1) {
+                clearInterval(lilyInterval)
+            } else {
+                addLily()
+            }
+        }
+        moveLily()
+
+        }, carSpeed * 5 * (index + 1))
+
+        setTimeout(() => {
+            clearInterval(lilyInterval)
+        }, carSpeed * 5 * (index + 1) + carSpeed * 2)
+    })
+}
 
 
     function handleMovement(event){
@@ -113,11 +217,6 @@ function init () {
         const right = "ArrowRight"
 
         removeFrog()
-
-        // if (!isCarMoving) {
-        //     isCarMoving = true
-        //     startCarMovements()
-        // }
 
         if (key === up && currentPosition >= width) {
             direction = "up"
@@ -135,6 +234,7 @@ function init () {
 
         position = currentPosition
         addFrog(currentPosition)
+        addLilyPads()
     }
 
 //! Events
